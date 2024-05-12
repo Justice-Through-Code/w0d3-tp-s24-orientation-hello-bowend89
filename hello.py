a71af10ce3fadaf4459ad20810ae657e8c720b97
Dennis <dbowentechpathways@gmail.com>
2024-05-12 16:27:54 -0400

    test_hello passed

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 def hello_world():
     # Remove the # in the line below and delete the word pass
-    # print("Hello World!")
+    print("Hello World!")
 
-    pass
    
 
 # Do not modify the code below this line
